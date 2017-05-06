f279d0be92f75cfbbf819c6f68c64f2426f0b4be
Anderson Marin <dam.marinramos@gmail.com>
2017-05-06 16:56:33 -0300

    Snake is now being fed, she just need to learn how to eat yet

--- snake.js
+++ snake.js
@@ -1,13 +1,20 @@
 var stage = document.getElementById('stage');
 var dimensions = stage.getContext('2d');
+var foodDropOf = stage.getContext('2d');
 var snakeSize = 10;
 var snakeX = 175;
 var snakeY = 175;
+var dropX = 0;
+var dropY = 0;
 var alive = true;
 var direction = 38;
+var initiated = false;
 var start = document.getElementById('start');
+var full = false;
 
 start.addEventListener('click', function() {
+	initiated = true;
+
 	init();
 });
 
@@ -31,60 +38,67 @@ var init = function() {
 		}
   		else
   		{
-  			return;
+  			return false;
   		}
 	}, 100);
 };
 
 var move = function(key) {
-	if(alive = false)
+	if(initiated != false)
 	{
-		return false;
-	}
+		if(alive == false)
+		{
+			return false;
+		}
 
-	if(key == 38) //up
-	{
-		snakeY -= 5;
-		direction = 38;
-	}
-	else if(key == 40) //down
-	{
-		snakeY += 5;
-		direction = 40;
-	}
-	else if(key == 37)//left
-	{
-		snakeX -= 5;
-		direction = 37;
-	}
-	else if(key == 39)//right
-	{
-		snakeX += 5;
-		direction = 39;
-	}
+		if(key == 38) //up
+		{
+			snakeY -= 5;
+			direction = 38;
+		}
+		else if(key == 40) //down
+		{
+			snakeY += 5;
+			direction = 40;
+		}
+		else if(key == 37)//left
+		{
+			snakeX -= 5;
+			direction = 37;
+		}
+		else if(key == 39)//right
+		{
+			snakeX += 5;
+			direction = 39;
+		}
 
-	clear(dimensions);
-	dimensions.fillRect(snakeX, snakeY, snakeSize, snakeSize);
-	alive = true;
+		clear(dimensions);
+		dimensions.fillRect(snakeX, snakeY, snakeSize, snakeSize);
+		alive = true;
+		feed(dimensions);
 
-	if(snakeY == 0)	
-	{
-		alive = false;
-	}
-	else if(snakeY == 345)
-	{
-		alive = false;
-	}
-	else if(snakeX == 0)
-	{
-		alive = false;
-	}
-	else if(snakeX == 345)
-	{
-		alive = false;
-	}
+		console.log(snakeX);
+		console.log(snakeY);
 
-	return alive;
+		if(snakeY == 0)	
+		{
+			alive = false;
+		}
+		else if(snakeY == 345)
+		{
+			alive = false;
+		}
+		else if(snakeX == 0)
+		{
+			alive = false;
+		}
+		else if(snakeX == 345)
+		{
+			alive = false;
+		}
+
+		return alive;
+	}
 };
 
 var clear = function(context) {
@@ -92,3 +106,21 @@ var clear = function(context) {
 
 	return true;
 };
+
+var kill = function(context) {
+	// later will kill the snake	
+};
+
+var feed = function(context) {
+	if(full != true)
+	{
+		dropX = Math.floor(Math.random() * 350) + 1;
+		dropY = Math.floor(Math.random() * 350) + 1;
+
+		full = true;
+	}
+
+	context.fillRect(dropX, dropY, snakeSize, snakeSize);
+
+	return true;
+}
